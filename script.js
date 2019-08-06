37860812e1ce75d2ac00eee18e1d47afd10d0097
danil <monstr14ekss@gmail.com>
2019-08-06 19:42:21 +0300

    New object appData

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 let money = +prompt('Ваш бюджет на месяц?', ''),
     time = prompt('Введите дату в формате YYYY-MM-DD', '2019-06-08');
+    
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
